13627b591a2a622b9a256096e2d7936137338822
veereshprabha834-hub <veereshprabha834@gmail.com>
2026-04-23 11:35:58 +0530

    Add files via upload

---
+++ oops.py
@@ -0,0 +1,25 @@
+class instagramvi:
+    def reel(self):
+        print("you can post the reel")
+
+
+print('veeresh - instagramv1')
+veeresh = instagramv1()
+veeresh.reel()
+
+class instagramv1(instagramv1):
+   def reel(self):
+       print("you can post the reel")
+
+class instagramv2(instagramv2):
+    def story(self):
+        print("you can upload a story")
+
+print('veeresh - instagram')
+veeresh = instagramv1()
+veeresh.reel()
+
+print('vp - instagramv2')
+vp = instagramv2()
+vp.reel()
+vp.story()
