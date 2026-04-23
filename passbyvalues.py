632a8bf7ed02c15a057ed27d09125c0bf8810bed
veereshprabha834-hub <veereshprabha834@gmail.com>
2026-04-23 11:36:24 +0530

    Add files via upload

---
+++ passbyvalues.py
@@ -0,0 +1,23 @@
+#int,float,str,list,tuple,set,dict
+'''
+def display(n):
+    n=n+10
+    print("inside",n)
+
+
+n=10
+display(n)
+print("outside",n)
+'''
+
+
+
+
+
+def display(n):
+    n=n+"lang"
+    print("inside",n)
+
+n="python"
+display(n)
+print("outside",n)
